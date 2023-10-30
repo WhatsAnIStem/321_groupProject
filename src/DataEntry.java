38f39f769e0538a9bfcef305dc8f930208066c07
Titania E <tiervin201@gmail.com>
2023-10-30 19:06:41 -0400

    got setup the UI loading...

--- src/DataEntry.java
+++ src/DataEntry.java
@@ -1,1 +1,14 @@
 package src;
+
+import src.shared_classes.FormUI;
+
+public class DataEntry{
+
+
+    public static void main(String[] args){
+        FormUI.launch(FormUI.class, args);
+    }
+
+
+
+}
