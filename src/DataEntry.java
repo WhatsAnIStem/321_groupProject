795fc5c022e3ef92b271fc8cf69b8ad0c327cb52
Titania E <tiervin201@gmail.com>
2023-10-30 19:38:59 -0400

    another commit, changed data entry screen entry point

--- src/DataEntry.java
+++ src/DataEntry.java
@@ -1,12 +1,30 @@
 package src;
 
-import src.shared_classes.FormUI;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class DataEntry{
+public class DataEntry extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(this.getClass().getResource("./MainForm.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+
+        
 
 
-    public static void main(String[] args){
-        FormUI.launch(FormUI.class, args);
+    }
+
+    public static void showDataEntryScreen(){
+
+
+
+
     }
 
 
---
+++ src/DataEntryDriver.java
@@ -0,0 +1,10 @@
+package src;
+
+public class DataEntryDriver{
+    
+    public static void main(String[] args){
+        // launch the form...
+        DataEntry.launch(DataEntry.class, args);
+    }
+
+}
