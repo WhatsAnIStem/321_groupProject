35dff6814674bee1d172962c31787edadd384a8a
dhamil-bytes <146766929+dhamil-bytes@users.noreply.github.com>
2023-11-01 13:34:23 -0400

    Create ReviewController.java

---
+++ src/ReviewController.java
@@ -0,0 +1,15 @@
+package src;
+
+import src.Review;
+import src.app_enums.app_status;
+import src.shared_classes.AccountCreation;
+import src.shared_classes.Workflow;
+
+
+public class ReviewController{
+
+    public static void main(String args[]) {
+        Review.launch(Review.class,args);
+    }
+
+}
