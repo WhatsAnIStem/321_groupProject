d643939b4e2f69e4e3124ad399ae0586b6c66100
dhamil-bytes <146766929+dhamil-bytes@users.noreply.github.com>
2023-11-03 17:44:47 -0400

    some changes to review

--- src/Review.java
+++ src/Review.java
@@ -20,13 +20,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
+// import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
-import javafx.scene.control.CheckBox;
+// import javafx.scene.control.CheckBox;
 
 import javafx.scene.input.InputEvent;
-import javafx.scene.text.Text;
+// import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.Locale;
@@ -60,14 +60,14 @@ public class Review extends Application {
     * [3] : valid_phone
      */
     private String[] changedFields;
-
+    private boolean needChange;
     private Button submitReview;
 
-    /* Constructor method that initializes status and ID of application. */
-    public Review(int app_ID, app_status status) { /* app_staus! */
+    /* Constructor method that initializes status and ID of application. 
+    public Review(int app_ID, app_status status) { // app_staus! 
         this.application_ID = app_ID; 
         this.application_status = status;
-    }
+    }*/
     
     /* Opening the UI. */
     @Override
@@ -80,6 +80,26 @@ public class Review extends Application {
         reviewStage.setTitle("Review Application Window");
         reviewStage.setScene(reviewScene);
         reviewStage.show();
+
+        EventHandler reviewHandler = new EventHandler<InputEvent>(){
+            public void handle(InputEvent IE){
+                //when clicked, get all the field entrys, validate, etc...
+                grabInfo();
+                //if all the fields pass validation...
+                if(validateApplication()){
+                    /* Application is valid. No need to edit data. */
+                    if (needChange) {
+                        editData(0, changedFields);
+                    }
+                }
+                //if not...
+                else{
+                    /* Fields fail in some way. */
+                    rejectApplication(0);
+                }
+            }
+
+        };
     }
 
     /* Accesses workflow given application ID. */
@@ -95,13 +115,8 @@ public class Review extends Application {
         /* Returns that block of AC data. */
         return null;
     }
-
-    /* Validates information. Returns true if info is correct, else false. */
-    private boolean validateApplication() { 
-        /* Grabs account creation information of an applicant from the UI. */
-        /* Validates information. */
-        /* If correct, true. Else, false. */
-
+    /* grabInfo - grabs data from UI. */
+    private boolean grabInfo(){
         /* Harvesting user input from the fxml file. */
         valid_dob = (DatePicker)reviewScene.lookup("#field_dob");
         valid_country = (TextField)reviewScene.lookup("#field_coo");
@@ -110,8 +125,16 @@ public class Review extends Application {
         /* Button for submitting review to the workflow. */
         submitReview = (Button)reviewScene.lookup("#button_submit");
 
-        /* Used to check whether or given information is valid. */
-        boolean is_valid = true;
+        needChange = false;
+
+        return true;
+    }
+
+    /* Validates information. Returns true if info is correct, else false. */
+    private boolean validateApplication() { 
+        /* Grabs account creation information of an applicant from the UI. */
+        /* Validates information. */
+        /* If correct, true. Else, false. */
 
         LocalDate curr = LocalDate.now();
         String[] localeCntry = Locale.getISOCountries();
@@ -127,48 +150,46 @@ public class Review extends Application {
          * submitReview: Button pressed after validations finalized.
          */
 
-        while (is_valid) {
-            /* Checking for valid input. */
-            if (valid_dob.toString().length() == 0 || valid_country.toString().length() == 0 || valid_email.toString().length() == 0 || valid_phone.toString().length() == 0) {
-                is_valid = false;
-            }
-
-            /* Checking to see if someone is 18 at time of review. */
-            if (curr.getYear() - valid_dob.getValue().getYear() == 18) {
-                /* 283-314 < 0, INVALID */
-                if(curr.getDayOfYear() - valid_dob.getValue().getDayOfYear() < 0) { is_valid = false; }
-            }
-            /* 2023-2006 = 17, INVALID */
-            else if (curr.getYear() - valid_dob.getValue().getYear() < 18) { is_valid = false; }
-
-            /* After importing Locale, we can check whether the country we entered is valid. */
-            Arrays.sort(localeCntry);
-            if (Arrays.binarySearch(localeCntry, valid_country.toString()) < 0) { is_valid = false; }
-            
-            /* If email is in correct format. Looks like this:
-             * [string1] @ [email] . [end]
-             */
-
-             /* Email: someEmail@gmail.com
-              * nameMail = [someEmail, gmail.com]. Length: 2
-              * nameMail[1] = "gmail.com"
-              * nameMail[1].split(".") = [gmail, com]. Length: 2
-              * Valid email!
-              */
-            String[] nameMail = valid_email.toString().split("@");
-            if (nameMail.length > 2) { is_valid = false; }
-            if (nameMail[1].split(".").length > 2) { is_valid = false; }
-
-            /* If phone is in correct format. Looks like this: 
-             * [1234567890]
-             */
-            if (valid_phone.toString().length() < 4 || valid_phone.toString().length() > 13) { is_valid = false; }
-
-            /* Breaks if all input is valid. */
-            break;
+        /* Checks for things being empty or not first. */
+        if (valid_dob == null || valid_country == null || valid_email == null || valid_phone == null) { needChange = true; return false; }
+        
+        /* Checking for valid input. */
+        if (valid_dob.toString().length() == 0 || valid_country.toString().length() == 0 || valid_email.toString().length() == 0 || valid_phone.toString().length() == 0) {
+            return false;
         }
 
-        return is_valid;
+        /* Checking to see if someone is 18 at time of review. */
+        if (curr.getYear() - valid_dob.getValue().getYear() == 18) {
+            /* 283-314 < 0, INVALID */
+            if(curr.getDayOfYear() - valid_dob.getValue().getDayOfYear() < 0) { return false; }
+        }
+        /* 2023-2006 = 17, INVALID */
+        else if (curr.getYear() - valid_dob.getValue().getYear() < 18) { return false; }
+
+        /* After importing Locale, we can check whether the country we entered is valid. */
+        Arrays.sort(localeCntry);
+        if (Arrays.binarySearch(localeCntry, valid_country.toString()) < 0) { return false; }
+        
+        /* If email is in correct format. Looks like this:
+            * [string1] @ [email] . [end]
+            */
+
+            /* Email: someEmail@gmail.com
+            * nameMail = [someEmail, gmail.com]. Length: 2
+            * nameMail[1] = "gmail.com"
+            * nameMail[1].split(".") = [gmail, com]. Length: 2
+            * Valid email!
+            */
+        String[] nameMail = valid_email.toString().split("@");
+        if (nameMail.length > 2) { return false; }
+        if (nameMail[1].split(".").length > 2) { return false; }
+
+        /* If phone is in correct format. Looks like this: 
+            * [1234567890]
+            */
+        if (valid_phone.toString().length() < 4 || valid_phone.toString().length() > 13) { return false; }
+
+        return true;
     }
 
     /* If validation returns false, we may 'correct' the application. */
@@ -183,8 +204,9 @@ public class Review extends Application {
          * [2] : valid_email
          * [3] : valid_phone
          */
-        
-        changedFields = new String[]{fields[0],fields[1],fields[2],fields[3]};
+        if (needChange) {
+            changedFields = new String[]{fields[0],fields[1],fields[2],fields[3]};
+        }
 
         /* Accesses Account Creation given an applicant's applicant ID. */
         return;
@@ -208,8 +230,4 @@ public class Review extends Application {
         /* Done from ReviewController. */
         return;
     }
-
-
-
-
 }
